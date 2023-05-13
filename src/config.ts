3db00cdceb944feb778d62968966b6f46b39ed78
elimelt <elimelt@uw.edu>
2023-05-12 23:37:07 -0700

    got email service working

---
+++ src/config.ts
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+
+const MY_EMAIL = process.env.MY_EMAIL as string;
+const MY_SENDER = process.env.MY_SENDER as string;
+const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY as string;
+const SERVER_URL = process.env.SERVER_URL as string;
+
+
+const config = { MY_EMAIL, MY_SENDER, SENDGRID_API_KEY, SERVER_URL }
+
+
+export default config;
---
+++ src/email.ts
@@ -0,0 +1,26 @@
+import sgMail from '@sendgrid/mail'
+import config from './config'
+
+export async function forward(name: string, email: string, feedback: string) {
+    
+
+    sgMail.setApiKey(config.SENDGRID_API_KEY)
+    const msg = {
+        to: config.MY_EMAIL,
+        from: config.MY_SENDER,
+        subject: `feedback from ${name}  |  ${email}`,
+        text: feedback,
+        html: `<strong>${feedback}</strong>`
+    }
+
+    try {
+        const result = await sgMail.send(msg)
+        console.log(result)
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+
+
+
