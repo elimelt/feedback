c81464b73bfe6aea8993307bb104a47ca1a9a0d5
Elijah Melton <98496503+elimelt@users.noreply.github.com>
2023-07-13 20:09:43 +0000

    testing job scheduler

--- src/logging.ts
+++ src/logging.ts
@@ -3,33 +3,7 @@ import * as path from 'path';
 import { Log, LogEntry } from './types';
 
 
-// append log data to the daily log file or create a new file if it doesn't exist
-export function logData(name: string, log: string) {
-  const date = new Date();
-  const logFileName = `${name}_${date.toISOString().slice(0, 10)}.txt`;
-  const logFilePath = path.join(__dirname, logFileName);
-
-  // currently getting error rror creating log file: 
-  // [Error: ENOENT: no such file or directory, open '/app/dist/logs/web_2023-07-12.txt']
-
-  // if the log file already exists
-  if (fs.existsSync(logFilePath)) {
-    fs.appendFile(logFilePath, log + '\n', (err) => {
-      if (err) {
-        console.error('Error writing to log file:', err);
-      }
-    });
-  } else { 
-    // new log file and append the log data
-    fs.writeFile(logFilePath, log + '\n', (err) => {
-      if (err) {
-        console.error('Error creating log file:', err);
-      }
-    });
-  }
-}
-
-export function saveData(prev: Log, entryData: any): Log {
+export const saveData = (prev: Log, entryData: any): Log => {
     const date = new Date();
     const datestring = date.toDateString() + " " + date.toTimeString()
     const newEntry: LogEntry = { timestamp: datestring, entry: entryData }
@@ -40,12 +14,22 @@ export function saveData(prev: Log, entryData: any): Log {
     return updatedLog    
 }
 
-export function generateLogReport(log: Log): any {
-    return {name: log.name, content: log.log}
-}
+export const generateLogReport = (log: Log): any => ( { name: log.name, content: log.log } )
 
-export function generateLogsReport(logs: Log[]): any {
-    let result: any = JSON.parse(JSON.stringify(logs))
 
-    return result;
+export const generateLogsReport = (logs: Log[]): any => JSON.parse(JSON.stringify(logs))
+
+// deprecated
+/*export*/ function logData(name: string, log: string) {
+  const date = new Date();
+  const logFileName = `${name}_${date.toISOString().slice(0, 10)}.txt`;
+  const logFilePath = path.join(__dirname, logFileName);
+
+  if (fs.existsSync(logFilePath)) 
+    fs.appendFile(logFilePath, log + '\n', (err) => 
+      err && console.error('Error writing to log file:', err))
+  else 
+    fs.writeFile(logFilePath, log + '\n', (err) => 
+      err && console.error('Error writing to log file:', err)
+    )
 }
